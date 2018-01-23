c0bed7b6fcca224eea50eb958dd5967dacdf55b4
danny-hazelcast <danny@hazelcast.com>
2018-01-23 22:46:25 +0200

    hzPyClient

--- HzClient.py
+++ HzClient.py
@@ -6,5 +6,10 @@ queueId = sys.argv[2]
 Id = sys.argv[3]
 clusterID = sys.argv[4]
 
+print("brokerIP = " + brokerIP)
+print("queueId = " + queueId)
+print("Id = " + Id)
+print("clusterID = " + clusterID)
+
 controller = HzControler.HzControler(brokerIP, queueId, Id, clusterID, sys.argv[5:])
 controller.start()
