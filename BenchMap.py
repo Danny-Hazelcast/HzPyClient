bdad0b081def5d626ba17f2e29a3422e1749406d
danny-hazelcast <danny@hazelcast.com>
2018-01-23 23:29:53 +0200

    hzPyClient

--- BenchMap.py
+++ BenchMap.py
@@ -6,6 +6,7 @@ class BenchMap(BenchBase.BenchBase):
     def __init__(self):
         super(BenchMap, self).__init__()
         self.map = None
+        self.keyDomain
 
     def init(self):
         self.map = self.client.get_map(self.name).blocking()
