0a8288064c4acfc4c0988a00e2bfdd8a2067fcea
danny-hazelcast <danny@hazelcast.com>
2018-01-24 08:01:58 +0200

    hzPyClient

--- MapGet.py
+++ MapGet.py
@@ -5,7 +5,7 @@ from random import randint
 class MapGet(BenchMap.BenchMap):
 
     def timeStep(self):
-        k = randint(0, int(self.keyDomain))
+        k = randint(0, int(self.keyDomain)-1)
         val = self.map.get(k)
 
         if val is None:
