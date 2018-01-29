1a30fc825b18faadb49265006d26114fa7112766
danny-hazelcast <danny@hazelcast.com>
2018-01-29 15:48:03 +0530

    hzPyClient

--- MapGet.py
+++ MapGet.py
@@ -1,4 +1,3 @@
-from hazelcast.exception import TimeoutError, TargetDisconnectedError
 
 import BenchMap
 from random import randint
@@ -9,12 +8,6 @@ class MapGet(BenchMap.BenchMap):
     def timeStep(self):
         k = randint(0, int(self.keyDomain) - 1)
         val = self.map.get(k)
-
-    # try:
-    #except TimeoutError as e:
-        #    return
-        #except TargetDisconnectedError as e:
-        #    return
 
         #if val is None:
         #    raise TypeError(" ")
