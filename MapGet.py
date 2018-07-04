d0fde354f6505e442ea409fdf0bc1844680f8e23
danny-hazelcast <danny@hazelcast.com>
2018-07-04 11:03:21 +0300

    hzPyClient

--- MapGet.py
+++ MapGet.py
@@ -10,11 +10,14 @@ class MapGet(BenchMap.BenchMap):
 
     def timeStep(self):
         k = randint(0, int(self.keyDomain) - 1)
-        try:
-            val = self.map.get(k)
-        except Exception as e:
-            print e
-            pass
+
+        #try:
+
+        val = self.map.get(k)
+
+        #except Exception as e:
+        #    print e
+        #    pass
 
 
         #if val is None:
